0c3ede7822a7cea35a404064c5050865fe256c32
kong-T <1812536092@qq.com>
2020-03-20 22:23:45 +0800

    温度转换程序

--- app/src/main/java/com/swufe/my/MainActivity.java
+++ app/src/main/java/com/swufe/my/MainActivity.java
@@ -3,12 +3,35 @@ package com.swufe.my;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView out;
+    EditText inp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        out  = (TextView) findViewById(R.id.showtext);
+        inp = (EditText)findViewById(R.id.inpText);
+
+        Button btn = (Button)findViewById(R.id.button);
+
+    }
+
+    public void btnClick(View btn){
+        showText();
+    }
+
+    private void showText() {
+        Log.i("show","temperature="+inp.getText().toString());
+        String t1 = (String) inp.getText().toString();
+        double t2 = Double.parseDouble(t1)*1.8+32;
+        out.setText("" + t2);
     }
 }
