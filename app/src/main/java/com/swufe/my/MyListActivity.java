11ce29c03074dabc06bd46a6e4b7cbcec459ceec
kong-T <1812536092@qq.com>
2020-05-20 16:45:56 +0800

    11.1 数据项的修改

--- app/src/main/java/com/swufe/my/MyListActivity.java
+++ app/src/main/java/com/swufe/my/MyListActivity.java
@@ -1,13 +1,21 @@
 package com.swufe.my;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.ListAdapter;
 import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MyListActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyListActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+    String TAG ="mylist";
+    ArrayAdapter adapter;
+    List<String> data = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,9 +23,23 @@ public class MyListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_my_list);
 
         ListView listview = (ListView) findViewById(R.id.mylist);
-        String data[] = {"111","2222"};
+        for (int i =0;i<=10;i++){
+            data.add("item"+i);
+        }
 
-         ListAdapter adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,data);
+         adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,data);
          listview.setAdapter(adapter);
+         listview.setEmptyView(findViewById(R.id.no_data));
+
+         listview.setOnItemClickListener(this);
+
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> listv, View view, int position, long id) {
+        Log.i(TAG,"onItemClick:position"+position);
+        Log.i(TAG,"onItemClick:parent"+listv);
+        //列表是通过方法的参数获得的
+        adapter.remove(listv.getItemAtPosition(position));
     }
 }
